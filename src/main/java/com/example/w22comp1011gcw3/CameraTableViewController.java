09e0ce3daaa070157b968ffc545484f3b1ad09ef
JaretWright <jaret.wright@georgiancollege.ca>
2022-02-07 11:01:37 -0500

    Updated the tableview controller to read from the DB and find the highest revenue camera

--- src/main/java/com/example/w22comp1011gcw3/CameraTableViewController.java
+++ src/main/java/com/example/w22comp1011gcw3/CameraTableViewController.java
@@ -2,8 +2,10 @@ package com.example.w22comp1011gcw3;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -34,11 +36,41 @@ public class CameraTableViewController implements Initializable {
 
     @FXML
     private TableColumn<Camera, Integer> unitsSoldColumn;
-    
+
+    @FXML
+    private Label highestRevenueLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ArrayList<Camera> cameras = DBUtility.getCamerasFromDB();
-        System.out.println("");
+        cameraIDColumn.setCellValueFactory(new PropertyValueFactory<>("cameraID"));
+        makeColumn.setCellValueFactory(new PropertyValueFactory<>("make"));
+        modelColumn.setCellValueFactory(new PropertyValueFactory<>("model"));
+        resolutionColumn.setCellValueFactory(new PropertyValueFactory<>("resolution"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        slrColumn.setCellValueFactory(new PropertyValueFactory<>("slr"));
+        unitsSoldColumn.setCellValueFactory(new PropertyValueFactory<>("unitsSold"));
+
+        tableView.getItems().addAll(DBUtility.getCamerasFromDB());
+        highestRevenueLabel.setText("Highest Revenue = " + getHighestRevenueString());
+    }
+
+    private String getHighestRevenueString()
+    {
+        if (tableView.getItems().size() == 0)
+            return "No cameras in the table";
+        else
+        {
+            Camera highRev = tableView.getItems().get(0);
+            for (Camera camera : tableView.getItems())
+            {
+                double highestRevenue = highRev.getPrice() * highRev.getUnitsSold();
+                double cameraRevenue = camera.getPrice() * highRev.getUnitsSold();
+                if (cameraRevenue>highestRevenue)
+                    highRev = camera;
+            }
+            double highestRevenue = highRev.getPrice() * highRev.getUnitsSold();
+            return (String.format("$%.2f, %s",highestRevenue, highRev));
+        }
     }
 }
 
